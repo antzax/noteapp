0222a338c773e2002bb88f1ae947b7af0a1ec7ab
anton <166389859+antzax@users.noreply.github.com>
2025-10-12 02:07:52 +0300

    Add user route

--- notebackend/models/note.js
+++ notebackend/models/note.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose')
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
     required: true,
+    minlength: 5,
   },
+
   important: Boolean,
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
+
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
--- notebackend/tests/test_helper.js
+++ notebackend/tests/test_helper.js
@@ -3,12 +3,12 @@ const Note = require('../models/note')
 const initialNotes = [
   {
     content: 'HTML is easy',
-    important: false
+    important: false,
   },
   {
     content: 'Browser can execute only JavaScript',
-    important: true
-  }
+    important: true,
+  },
 ]
 
 const nonExistingId = async () => {
@@ -21,9 +21,18 @@ const nonExistingId = async () => {
 
 const notesInDb = async () => {
   const notes = await Note.find({})
-  return notes.map(note => note.toJSON())
+  return notes.map((note) => note.toJSON())
+}
+const User = require('../models/user')
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((u) => u.toJSON())
 }
 
 module.exports = {
-  initialNotes, nonExistingId, notesInDb
+  initialNotes,
+  nonExistingId,
+  notesInDb,
+  usersInDb,
 }
